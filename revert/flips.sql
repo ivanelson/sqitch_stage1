f35e034d0fe8c6824dec7400d891c697b25d4e5a
ivan nunes <ivan.nunes@armazemparaiba.com.br>
2016-01-14 20:45:04 -0300

    Add flips table.

---
+++ revert/flips.sql
@@ -0,0 +1,8 @@
+-- Revert flipr:flips from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+   DROP TABLE flipr.flips;
+
+COMMIT;
---
+++ verify/flips.sql
@@ -0,0 +1,13 @@
+-- Verify flipr:flips on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+SELECT id
+     , nickname
+     , body
+     , timestamp
+  FROM flipr.flips
+ WHERE FALSE;
+
+ROLLBACK;
